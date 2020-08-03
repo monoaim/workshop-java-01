0bb77a6bfaba5dc4f4764f87b740f8c6fa3a63eb
Araya Siriadun <araya.siriadun@gmail.com>
2020-08-03 11:33:32 +0700

    HelloTest

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+import java.awt.HeadlessException;
+
+public class Hello {
+
+    public String sayHi() {
+        return "Hello";
+    }
+
+    public String sayHi(String str) {
+        return "Hello" + " " + str;
+    }
+}
